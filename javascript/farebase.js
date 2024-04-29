40f60c7d7e012871f64097bcb1d354b4ddf6e490
Daniel Almeida <129712939+DanielGT89@users.noreply.github.com>
2024-04-29 00:50:57 -0300

    Update farebase.js

--- javascript/farebase.js
+++ javascript/farebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAaGmWKqJaWhI2AmqmZb6xAi04rVSLJTYY",
-    authDomain: "cadastro-c3318.firebaseapp.com",
-    projectId: "cadastro-c3318", 
-    storageBucket: "cadastro-c3318.appspot.com",
-    messagingSenderId: "962303898937",
-    appId: "1:962303898937:web:745ac94cdbd7bd067524e8"
+    apiKey: "jfasjdkfjkjijijieioklçsk",
+    authDomain: ".firebaseapp.com",
+    projectId: "8", 
+    storageBucket: ".appspot.com",
+    messagingSenderId: "9",
+    appId: "1:97:web:78"
 }
 
 firebase.initializeApp(firebaseConfig);

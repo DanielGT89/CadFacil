24e9f1d0697cf62296013ef7383bfff62603b639
Daniel Almeida <129712939+DanielGT89@users.noreply.github.com>
2024-04-29 00:59:49 -0300

    Update register.js

--- javascript/register.js
+++ javascript/register.js
@@ -1,6 +1,6 @@
 function register() {
 
-    firebase.auth().signInWithEmailAndPassword("danielmmmelquiades@gmail.com", "123456").then(response => { console.log('success', response); }).catch(error => { console.log('error', error); });
+    firebase.auth().signInWithEmailAndPassword("", "").then(response => { console.log('success', response); }).catch(error => { console.log('error', error); });
 
 
     //window.location.href="cadastra.html"
